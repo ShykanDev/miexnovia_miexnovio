f8354e02eea7105ef79275b29a3d85018b60cb49
awscodeapp@gmail.com <awscodeapp@gmail.com>
2024-11-07 20:23:03 -0600

    Fixed page url base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/miexnovia_miexnovio/',
+  base: '/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
